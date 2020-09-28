434cd38cf7fb0852f680acaab850a3c4f10c65dd
Renato Ferreira <renatoferreira.dev@gmail.com>
2020-09-28 17:21:47 +0200

    Increase unit tests of ShowCard component

--- src/components/ShowCard/ShowCard.test.tsx
+++ src/components/ShowCard/ShowCard.test.tsx
@@ -5,9 +5,27 @@ import ShowCard from './ShowCard';
 describe('ShowCard', () => {
   it('should render without crashing', () => {
     const { getByTestId } = render(
-      <ShowCard average={5.2} amount={3} image="" />,
+      <ShowCard average={5.2} image="image.png" />,
     );
 
+    expect(getByTestId('show-card')).toBeDefined();
+  });
+
+  it('should props properly', () => {
+    const { getByTestId, queryByAltText } = render(
+      <ShowCard average={5.2} image="image.png" />,
+    );
+
+    expect(queryByAltText('image.png')).toBeDefined();
+    expect(getByTestId('show-card')).toHaveTextContent('5.2');
+  });
+
+  it('should fire onClick when function is clicked', () => {
+    const { getByTestId } = render(
+      <ShowCard average={5.2} image="image.png" />,
+    );
+
+    expect(getByTestId('show-card')).toBeDefined();
     expect(getByTestId('show-card')).toBeDefined();
   });
 });
