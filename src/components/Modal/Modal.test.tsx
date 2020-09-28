24e0af83596cf5dde4a334b577bbf94c59888494
Renato Ferreira <renatoferreira.dev@gmail.com>
2020-09-28 16:58:09 +0200

    Increase unit tests of Modal component

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,11 +1,59 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import Modal from './Modal';
+
+const modalContent = 'Modal Content';
 
 describe('Modal', () => {
   it('should render without crashing', () => {
-    const { getByTestId } = render(<Modal open title="Modal title">Modal content</Modal>);
+    const { getByTestId } = render(
+      <Modal open title="Modal title">
+        {modalContent}
+      </Modal>,
+    );
 
     expect(getByTestId('modal')).toBeDefined();
+  });
+
+  it('should children content', () => {
+    const { getByTestId } = render(
+      <Modal open title="Modal title">
+        {modalContent}
+      </Modal>,
+    );
+
+    expect(getByTestId('modal')).toHaveTextContent(modalContent);
+  });
+
+  it('should fire onClose function whne close icon is clicked', () => {
+    const onClose = jest.fn();
+    const { queryByAltText } = render(
+      <Modal open title="Modal title" onClose={onClose}>
+        {modalContent}
+      </Modal>,
+    );
+    const closeIcon = queryByAltText('close') as HTMLImageElement;
+
+    fireEvent.click(closeIcon);
+
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render props properly', () => {
+    const title = 'Modal title';
+    const { queryByTestId, rerender } = render(
+      <Modal title={title}>{modalContent}</Modal>,
+    );
+
+    expect(queryByTestId('modal')).toBeNull();
+
+    rerender(
+      <Modal open title="Modal title">
+        {modalContent}
+      </Modal>,
+    );
+
+    expect(queryByTestId('modal')).toBeDefined();
+    expect(queryByTestId('modal')).toHaveTextContent(title);
   });
 });
