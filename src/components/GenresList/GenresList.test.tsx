c8e8c8117ee9cade8bfab2e2dda926a87d672af4
Renato Ferreira <renatoferreira.dev@gmail.com>
2020-09-28 16:38:22 +0200

    Increate unit test for GenresList component

--- src/components/GenresList/GenresList.test.tsx
+++ src/components/GenresList/GenresList.test.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import GenresList from './GenresList';
+
+const genres = ['Action', 'Documentary'];
 
 describe('GenresList', () => {
   it('should render without crashing', () => {
-    const { getByTestId } = render(
-      <GenresList genres={['Action', 'Documentary']} />,
-    );
+    const { getByTestId } = render(<GenresList genres={genres} />);
 
     expect(getByTestId('genres-list')).toBeDefined();
+  });
+
+  it('should specified genres', () => {
+    const { getByText } = render(<GenresList genres={genres} />);
+
+    expect(getByText(genres[0])).toBeDefined();
+    expect(getByText(genres[1])).toBeDefined();
+  });
+
+  it('should fire onClick when genre is selected', () => {
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <GenresList genres={genres} onSelect={onClick} />,
+    );
+
+    fireEvent.click(getByText(genres[0]));
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
