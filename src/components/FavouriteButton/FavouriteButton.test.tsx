c6b7a09bc501e3b4c9c29ae435423e6f149d4060
Renato Ferreira <renatoferreira.dev@gmail.com>
2020-09-28 16:22:28 +0200

    Increate unit test for FavouriteButton component

--- src/components/FavouriteButton/FavouriteButton.test.tsx
+++ src/components/FavouriteButton/FavouriteButton.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import FavouriteButton from './FavouriteButton';
 
 describe('FavouriteButton', () => {
@@ -9,5 +9,31 @@ describe('FavouriteButton', () => {
     );
 
     expect(getByTestId('favourite-button')).toBeDefined();
+  });
+
+  it('should fire onClick function when clicked', () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render(
+      <FavouriteButton text="Button" isFavourited onClick={onClick} />,
+    );
+
+    fireEvent.click(getByTestId('favourite-button'));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render correct icon when `isFavourited` prop changes', () => {
+    const { queryByAltText, rerender } = render(
+      <FavouriteButton text="Button" isFavourited={false} />,
+    );
+    const heartIcon = 'heart.svg';
+    const heartShapeIcon = 'heart-shape.svg';
+
+    expect(queryByAltText(heartIcon)).toBeDefined();
+    expect(queryByAltText(heartShapeIcon)).toBeNull();
+
+    rerender(<FavouriteButton text="Button" isFavourited />);
+
+    expect(queryByAltText(heartShapeIcon)).toBeDefined();
+    expect(queryByAltText(heartIcon)).toBeNull();
   });
 });
